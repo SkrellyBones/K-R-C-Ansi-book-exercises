53367d31735ec17a09f9ad346f11ee7b600ee7c9
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-01 09:43:37 -0800

    updated any.c

--- chapter_2/any.c
+++ chapter_2/any.c
@@ -1,15 +1,21 @@
+/* returns the first location in a string s1
+where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.*/
+#include <stdio.h>
 void any(char s1[], char s2[]){
     int i, j, k, match;
-    for(i = 0, j = 0; s1[i] != '\0'; i++){
+    for (i = 0; s1[i] != '\0'; i++) {
         match = 0;
-        for(k = 0; s2[k] != '\0'; k++){
-            if(s1[i] == s2[k]){
+        for (j = 0; s2[j] != '\0'; j++) {
+            if (s1[i] == s2[j]) {
                 match = 1;
                 break;
             }
         }
-        if(!match){
-            s1[j++] = s1[i];
+        if (match) {
+            printf("First occurrence at index: %d\n", i);
+            return;
         }
     }
+    printf("-1\n");
+    
 }
