4af1680f8be65fbc06667e4a2273ed93d6f958c7
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-04 22:15:31 -0800

    modified atofextend.c

--- chapter_4/atofextend.c
+++ chapter_4/atofextend.c
@@ -1,32 +1,31 @@
-//extend atof to handle scientifc notation
+// extend atof to handle scientifc notation
 #include <ctype.h>
-double atof(char s[]){
+double atof(char s[]) {
     double val, power, exp_power;
     int i, sign, exp_sign, exponent;
-    for(i = 0; isspace(s[i]); i++) /* skip white space */
-        ;
-    sign = (s[i] == '-') ? -1 : 1;
-    if(s[i] == '+' || s[i] == '-')
+    for (i = 0; isspace(s[i]); i++)
+        sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
         i++;
-    for(val = 0.0; isdigit(s[i]); i++)
+    for (val = 0.0; isdigit(s[i]); i++)
         val = 10.0 * val + (s[i] - '0');
-    if(s[i] == '.')
+    if (s[i] == '.')
         i++;
-    for(power = 1.0; isdigit(s[i]); i++){
+    for (power = 1.0; isdigit(s[i]); i++) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10;
     }
-    if(s[i] == 'e' || s[i] == 'E'){
+    if (s[i] == 'e' || s[i] == 'E') {
         i++;
         exp_sign = (s[i] == '-') ? -1 : 1;
-        if(s[i] == '+' || s[i] == '-')
+        if (s[i] == '+' || s[i] == '-')
             i++;
-        for(exponent = 0; isdigit(s[i]); i++)
+        for (exponent = 0; isdigit(s[i]); i++)
             exponent = 10 * exponent + (s[i] - '0');
         exp_power = 1.0;
-        for(int j = 0; j < exponent; j++)
+        for (int j = 0; j < exponent; j++)
             exp_power *= 10;
-        if(exp_sign == -1)
+        if (exp_sign == -1)
             val /= exp_power;
         else
             val *= exp_power;
