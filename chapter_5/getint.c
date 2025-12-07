fdc284a520c27189f3a6a2ca72fbc99d9c52778c
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-07 12:19:59 -0800

    updated getint.c

--- chapter_5/getint.c
+++ chapter_5/getint.c
@@ -23,4 +23,29 @@ int getint(int *pn) {
     return c;
 }
 
+#define BUFSIZE 100
+int buf;
+int bufp = 0;
+int getch(void) /* get a (possibly pushed-back) character, modified to assume
+                   there will never be more than one */
+{
+    if (bufp == 1) {
+        bufp = 0;
+        return buf;
+    }
+    return getchar();
+}
+void ungetch(int c)
+/* push character back on input, modified to assume there will never be more
+   than one pushed back character */
+{
+    if (bufp == 1)
+        printf("ungetch: too many characters\n");
+    if (c == EOF)
+        return;
+    else {
+        buf = c;
+        bufp = 1;
+    }
+}
 
