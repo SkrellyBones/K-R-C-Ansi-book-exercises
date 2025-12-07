79f1b9b00b9830a7d7e6f617649941ce00a4b9ff
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-07 14:01:07 -0800

    updated strlen.c

--- chapter_5/strlen.c
+++ chapter_5/strlen.c
@@ -1,7 +1,15 @@
 /* strlen: return length of string s */
+#include <stdio.h>
 int strLen(char *s) {
     int n;
     for (n = 0; *s != '\0'; s++)
         n++;
     return n;
+}
+int main(){
+    char *s = "Hello World";
+    int a = strLen(s);
+    printf("String: %s\n", s);
+    printf("Length of string: %d\n", a);
+
 }
