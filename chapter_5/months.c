0235aa063a9266180833000583ed502ef1eae819
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-08 04:08:22 -0800

    updated months.c to use pointers instead of array indexing

--- chapter_5/months.c
+++ chapter_5/months.c
@@ -2,28 +2,33 @@
 static char daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
-
 int day_of_year(int year, int month, int day) {
     int i, leap;
+    char *p;
     if (year < 1 || month < 1 || month > 12)
         return -1;
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    if (day < 1 || day > daytab[leap][month])
+     p = *(daytab + leap);  
+    if (day < 1 || day > *(p + month))
         return -1;
     for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+        day += *(p + i);
     return day;
 }
 int month_day(int year, int yearday, int *pmonth, int *pday) {
     int i, leap;
+    char *p; 
     if (year < 1 || pmonth == NULL || pday == NULL)
         return -1;
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    p = *(daytab + leap);  // pointer to the row
     if (yearday < 1 || yearday > 365 + leap)
         return -1;
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
+    i = 1;
+    while (yearday > *(p + i))
+        yearday -= *(p + i++);
     *pmonth = i;
     *pday = yearday;
-    return 0; 
+
+    return 0;
 }
