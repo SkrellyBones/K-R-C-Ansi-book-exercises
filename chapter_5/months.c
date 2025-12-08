c7ea589c83f81601147257868be2b8b62b1fdbd9
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-08 03:03:55 -0800

    added error checking to months.c

--- chapter_5/months.c
+++ chapter_5/months.c
@@ -1,21 +1,29 @@
+#include <stdio.h>
 static char daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
-/* day_of_year: set day of year from month & day */
+
 int day_of_year(int year, int month, int day) {
     int i, leap;
+    if (year < 1 || month < 1 || month > 12)
+        return -1;
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    if (day < 1 || day > daytab[leap][month])
+        return -1;
     for (i = 1; i < month; i++)
         day += daytab[leap][i];
     return day;
 }
-/* month_day: set month, day from day of year */
-    void
-    month_day(int year, int yearday, int *pmonth, int *pday) {
+int month_day(int year, int yearday, int *pmonth, int *pday) {
     int i, leap;
+    if (year < 1 || pmonth == NULL || pday == NULL)
+        return -1;
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    if (yearday < 1 || yearday > 365 + leap)
+        return -1;
     for (i = 1; yearday > daytab[leap][i]; i++)
         yearday -= daytab[leap][i];
     *pmonth = i;
     *pday = yearday;
+    return 0; 
 }
