3b2934bb5aa2308801276ec62b4dc616b360191f
Caleb Triplett <43258524+SkrellyBones@users.noreply.github.com>
2025-12-10 00:19:35 -0800

    updated binarytree.c

--- chapter_6/binarytree.c
+++ chapter_6/binarytree.c
@@ -1,6 +1,16 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+struct tnode {
+    /* the tree node: */
+    char *word;
+    /* points to the text */
+    int count;
+    /* number of occurrences */
+    struct tnode *left;
+    /* left child */
+    struct tnode *right; /* right child */
+};
 #define MAXWORD 100
 struct tnode *addtree(struct tnode *, char *);
 void treeprint(struct tnode *);
@@ -15,4 +25,57 @@ int main() {
             root = addtree(root, word);
     treeprint(root);
     return 0;
+}
+struct tnode *talloc(void);
+char *strdup(char *);
+/* addtree: add a node with w, at or below p */
+struct treenode *addtree(struct tnode *p, char *w) {
+    int cond;
+    if (p == NULL) {
+        /* a new word has arrived */
+        p = talloc();
+        /* make a new node */
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    } else if ((cond = strcmp(w, p->word)) == 0)
+        p->count++;
+    /* repeated word */
+    else if (cond < 0)
+        /* less than into left subtree */
+        p->left = addtree(p->left, w);
+    else
+        /* greater than into right subtree */
+        p->right = addtree(p->right, w);
+    return p;
+}
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p) {
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+char *strdup(char *);
+/* addtree: add a node with w, at or below p */
+struct treenode *addtree(struct tnode *p, char *w) {
+    int cond;
+    if (p == NULL) {
+        /* a new word has arrived */
+        p = talloc();
+        /* make a new node */
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    } else if ((cond = strcmp(w, p->word)) == 0)
+        p->count++;
+    /* repeated word */
+    else if (cond < 0)
+        /* less than into left subtree */
+        p->left = addtree(p->left, w);
+    else
+        /* greater than into right subtree */
+        p->right = addtree(p->right, w);
+    return p;
 }
